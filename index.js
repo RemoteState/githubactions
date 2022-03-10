7fcbd6cf3763843d74fdffaac9e818e0d2a682ff
gaurav.bhardwaj <gaurav.bhardwaj@nickelfox.com>
2022-03-10 13:35:10 +0530

    world

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res)=>{
-    return res.send("Hello World")
+    return res.send("Welcome to my World")
 })
 
 app.listen(3000, ()=>{
