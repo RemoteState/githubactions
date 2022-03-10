cadb8bc31c64b7812b003a6a9a29537dbf3f92ca
gaurav.bhardwaj <gaurav.bhardwaj@nickelfox.com>
2022-03-10 13:22:08 +0530

    skdk

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res)=>{
-    return res.send("Hello Munna")
+    return res.send("Hello World")
 })
 
 app.listen(3000, ()=>{
